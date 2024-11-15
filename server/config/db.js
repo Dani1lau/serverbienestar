2ab727afa1a600e2970c116b4c4356a54a748bec
Dani1lau <soydanielra16@gmail.com>
2024-11-14 20:21:14 -0500

    se arreglo el db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,18 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS,{
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    dialectOptions: {
+export const sequelize = new Sequelize(
+    process.env.DB_SCHEMA,         // El nombre de la base de datos
+    process.env.DB_USER,           // El usuario de la base de datos
+    process.env.DB_PASS,           // La contraseña del usuario
+    {
+        host: process.env.DB_HOST,  // El host (autorack.proxy.rlwy.net)
+        port: process.env.DB_PORT,  // El puerto (57669)
+        dialect: "mysql",           // Usamos MySQL como dialecto
+        dialectOptions: {
+            // Si tienes alguna opción adicional para el dialecto, añádela aquí
+        }
     }
-})
+);
